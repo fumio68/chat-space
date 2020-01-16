fe07763616fa03dedcd3a716a19fb5038db26683
fumio68 <rgkk.5123@gmail.com>
2020-01-16 13:24:53 +0900

    install CarrierwaveGem

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,6 +3,16 @@ class MessagesController < ApplicationController
   end
 
   def create
+    if Message.create(message_params)
+      redirect_to group_messages_path #あとでグループidがわかるように差し替え
+    else
+      render :index
+    end
   end
-  
+
+  private
+  def message_params
+    params.require(:message).permit(:content, :image)
+  end
+
 end
