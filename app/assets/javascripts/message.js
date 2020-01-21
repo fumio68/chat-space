b7ef6148acf142d080a9443f30298e8e23afd6c8
fumio68 <rgkk.5123@gmail.com>
2020-01-21 12:43:24 +0900

    add reloadMessage to message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -67,4 +67,25 @@ $(function(){
       $(".main-chat__form__box__submit-btn").removeAttr("disabled");
     })
   })
+
+  var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    last_message_id = $('.main-chat__message-list__message:last').data("message-id");
+    $.ajax({
+      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+      url: "api/messages",
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      // dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      console.log('success');
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  };
+  
 });
